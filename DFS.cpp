98b03ce8cb65612024d9d7ae1a1535008a7a2ed0
Lâm Huệ Trung <100610960+LHT-110121255@users.noreply.github.com>
2022-11-22 16:10:51 +0700

    Buổi 3 - BTH4

--- DFS.cpp
+++ DFS.cpp
@@ -6,7 +6,7 @@ using namespace std;
 #define TRUE 1
 #define FALSE 0
 
-int G[MAX][MAX], n, chuaxet[MAX], S[MAX], K;
+int G[MAX][MAX], n, chuaxet[MAX], S[MAX], K, m;
 
 void Init() {
 	freopen("matran.txt", "r", stdin);
@@ -16,8 +16,6 @@ void Init() {
 		cin >> G[i][j];
 }
 
-
-	
 void DFS(int x) {
 	int index = 0;
 	S[index] = x;
@@ -36,6 +34,59 @@ void DFS(int x) {
 			}
 	}
  }
+ 
+// chua xong
+void BFS_2(int x) {
+	int index = 0;
+	S[index] = x; //[1,]
+	K = S[index];
+	for (int g = 1; g <= n; g++){
+		if(chuaxet[K] == FALSE ) 
+		{
+			chuaxet[K] = TRUE;
+		for(int i = 1; i <= n; i++)
+			if(G[i][K] == 1 && chuaxet[i] == FALSE) 
+			{
+				index++;
+				S[index] = i;
+			} 
+		} else {
+			K = S[g++];
+		}
+					
+	}
+	for(int i = 0; i < n; i++)
+		cout << S[i] << "\t";
+ }
+ 
+ // theo cách th?y
+ 
+ int BFS(int v)
+{
+	int T[20];
+	int t=0;
+	T[t]=v;
+	t++;
+	d[v]=-1;
+	while(t!=0)
+	{
+		int x=T[0];
+		printf("%d ",x);
+		
+		for(int i=0 ; i<t ; i++)
+		T[i]=T[i+1];
+		t--;
+		for(int i=1 ; i<=n ; i++)
+		{
+		if(a[x][i] != 0 && d[i]==0)
+		{
+		T[t]=i;
+		t++;
+		d[i]=-1;			
+		}
+		}
+	}
+}
 int main() {
 	int r;
 	cout << " Nhap dinh can duyet: "; 
@@ -43,6 +94,6 @@ int main() {
 	Init();
 	for(int i = 1; i <= n; i++)
 		chuaxet[i] = FALSE;
-	cout << "Duyet DFS cua dinh " << r << ":\n";
-	DFS(r);
+	printf("\nKet qua duyet BFS(%d) la: ", x);
+	BFS(x);
 }
